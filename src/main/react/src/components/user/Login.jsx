c0b12dc8b58d4baf00c3a6c1fac3f8f729e3db5f
小轩 <709027500@qq.com>
2020-12-08 17:04:51 +0800

    login

--- src/main/react/src/components/user/Login.jsx
+++ src/main/react/src/components/user/Login.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
+import '../../css/css2.css';
+import socket from '../../images/socket.svg';
 import axios from 'axios';
 import cookie from 'react-cookies'
-import { withRouter } from 'react-router-dom';
-class Login extends React.Component {
+import {Form, Input, Button, Checkbox} from 'antd';
+import {UserOutlined, LockOutlined} from '@ant-design/icons';
+
+
+export default class Login extends React.Component {
 
     constructor(props) {
         super(props);
@@ -13,18 +17,16 @@ class Login extends React.Component {
     }
 
 
-
-    login() {
-
+    onFinish = (values) => {
         axios.post('http://localhost/user/login', {
-            account: this.account.value,
-            password: this.password.value,
+            account: values.account,
+            password: values.password,
         }).then((response) => {
             var result = response.data.message;
             var token = response.data.data;
             if (result == 'Success') {
                 let inFifteenMinutes = new Date(new Date().getTime() + 7 * 24 * 3600 * 1000);
-                cookie.save('token', token, { expires: inFifteenMinutes })
+                cookie.save('token', token, {expires: inFifteenMinutes})
                 this.props.history.push('/index')
             } else if (result == 'LoginRefuse') {
                 alert('账号已被封禁')
@@ -33,44 +35,84 @@ class Login extends React.Component {
             }
 
         });
-    }
 
+    };
 
     register() {
         this.props.history.push('/register')
     }
 
     render() {
-        return <div>
-            <div className="modal-dialog" style={{ marginTop: '10%' }}>
-                <div className="modal-content">
-                    <div className="modal-header">
+        return (
 
-                        <h4 className="modal-title text-center" >{this.state.title}</h4>
-                    </div>
-                    <div className="modal-body" >
-                        <div className="form-group">
+            <div className="container-fluid">
+                <div className="row" style={{marginBottom: '5%', paddingTop: '20px', minHeight: '200px'}}>
+                    <div className="col-xl-3"></div>
+                    <div className="col-xl-6"><img src={socket} alt="" width="100%" height="200px"/></div>
+                    <div className="col-xl-3">
+                        <div>
 
-                            <input type="text" className="form-control" placeholder="账号" ref={input => this.account = input} autoComplete="off" />
                         </div>
-                        <div className="form-group">
-
-                            <input type="password" className="form-control" placeholder="密码" ref={input => this.password = input} autoComplete="off" />
-                        </div>
-                    </div>
-                    <div className="modal-footer">
-                        <div className="form-group">
-                            <button type="button" style={{ outline: 'none' }} className="btn btn-primary form-control" onClick={() => this.login()}>登录</button>
-                        </div>
-                        <div className="form-group">
-                            <button type="button" style={{ outline: 'none' }} className="btn btn-success form-control" onClick={() => this.register()}>注册</button>
-                        </div>
-
                     </div>
                 </div>
-            </div>
-        </div>
+                <div className="row">
+                    <div className="col-xl-3"></div>
+                    <div className="col-xl-6">
+                        <div>
+                            <Form
+                                name="normal_login"
+                                className="login-form"
+                                initialValues={{remember: true}}
+                                onFinish={this.onFinish}
+                            >
+                                <Form.Item
+                                    name="account"
+                                    rules={[{required: true, message: 'Please input your Username!'}]}
+                                >
+                                    <Input prefix={<UserOutlined className="site-form-item-icon"/>}
+                                           placeholder="Username"/>
+                                </Form.Item>
+                                <Form.Item
+                                    name="password"
+                                    rules={[{required: true, message: 'Please input your Password!'}]}
+                                >
+                                    <Input.Password
+                                        prefix={<LockOutlined className="site-form-item-icon"/>}
+                                        type="password"
+                                        placeholder="Password"
+                                    />
+                                </Form.Item>
+                                <Form.Item>
+                                    <Form.Item name="remember" valuePropName="checked" noStyle>
+                                        <Checkbox>记住我</Checkbox>
+                                    </Form.Item>
+
+                                    <a className="login-form-forgot" href="">
+                                        忘记密码
+                                    </a>
+                                </Form.Item>
+
+                                <Form.Item>
+                                    <Button type="primary" htmlType="submit" className="login-form-button"
+                                            style={{marginBottom: '20px'}}>
+                                        登录
+                                    </Button>
+                                    <Button type="primary" htmlType="button" className="login-form-button"
+                                            onClick={() => this.register()}>
+                                        立即注册
+                                    </Button>
+                                </Form.Item>
+                            </Form>
+                        </div>
+                    </div>
+                    <div className="col-xl-3"></div>
+                </div>
+
+
+            </div>);
+
+
     }
 }
 
-export default withRouter(Login);
+
--- src/main/react/src/components/user/Register.jsx
+++ src/main/react/src/components/user/Register.jsx
@@ -1,37 +1,102 @@
-import React, { Component } from 'react';
+import React from 'react';
+import '../../css/css2.css';
+import socket from '../../images/socket.svg';
+import axios from 'axios';
+import {Form, Input, Button, Checkbox} from 'antd';
+import {UserOutlined, LockOutlined} from '@ant-design/icons';
+import {NavLink} from "react-router-dom";
+
+
 export default class Register extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: '注册',
+        }
+    }
+
+
+    onFinish = (values) => {
+        axios.post('http://localhost/user/register', {
+            account: values.account,
+            password: values.password,
+        }).then((response) => {
+            const result = response.data.message;
+            if (result == 'Success') {
+                this.props.history.push('/login')
+            } else {
+                alert('注册失败');
+            }
+        });
+
+    };
+
     render() {
-        return <div id="registerSpace" className="modal-dialog" style={{ marginTop: '10%' }}>
-            <div className="modal-content">
-                <div className="modal-header">
+        return (
 
-                    <h4 className="modal-title text-center" id="registerLabel">注册</h4>
-                </div>
-                <div className="modal-body" id="model-body1">
+            <div className="container-fluid">
+                <div className="row" style={{marginBottom: '5%', paddingTop: '20px', minHeight: '200px'}}>
+                    <div className="col-xl-3"></div>
+                    <div className="col-xl-6"><img src={socket} alt="" width="100%" height="200px"/></div>
+                    <div className="col-xl-3">
+                        <div>
 
-                    <div className="form-group">
-
-                        <input type="text" id="registerAccount" className="form-control" placeholder="账号" autoComplete="off" />
-                    </div>
-                    <div className="form-group">
-
-                        <input type="password" id="registerPassword" className="form-control" placeholder="密码" autoComplete="off" />
+                        </div>
                     </div>
                 </div>
+                <div className="row">
+                    <div className="col-xl-3"></div>
+                    <div className="col-xl-6">
+                        <div>
+                            <Form
+                                name="normal_login"
+                                className="login-form"
+                                initialValues={{remember: true}}
+                                onFinish={this.onFinish}
+                            >
+                                <Form.Item
+                                    name="account"
+                                    rules={[{required: true, message: 'Please input your Username!'}]}
+                                >
+                                    <Input prefix={<UserOutlined className="site-form-item-icon"/>}
+                                           placeholder="Username"/>
+                                </Form.Item>
+                                <Form.Item
+                                    name="password"
+                                    rules={[{required: true, message: 'Please input your Password!'}]}
+                                >
+                                    <Input.Password
+                                        prefix={<LockOutlined className="site-form-item-icon"/>}
+                                        type="password"
+                                        placeholder="Password"
+                                    />
+                                </Form.Item>
+                                <Form.Item>
 
-                <div className="modal-footer">
 
-                    <div id="goLogin" className="pull-left" style={{ cursor: 'pointer' }}  >已有账号?直接登录</div>
+                                    <NavLink className="login-form-forgot" to="/login">
+                                        已有账号？去登录
+                                    </NavLink>
+                                </Form.Item>
 
-                    <button type="button" id="register" style={{ width: '100%', outline: 'none' }} className="btn btn-primary form-control">注册
-                    </button>
-
-
+                                <Form.Item>
+                                    <Button type="primary" htmlType="submit" className="login-form-button"
+                                            style={{marginBottom: '20px'}}>
+                                        立即注册
+                                    </Button>
+                                </Form.Item>
+                            </Form>
+                        </div>
+                    </div>
+                    <div className="col-xl-3"></div>
                 </div>
 
-            </div>
-        </div>
+
+            </div>);
+
 
     }
 }
+
+
