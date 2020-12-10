2fd8de8e720a40652383df57f2b1c74c300d984b
小轩 <709027500@qq.com>
2020-12-10 17:40:09 +0800

    修改密码

--- src/main/java/com/xuan/dtrun/controller/TaskController.java
+++ src/main/java/com/xuan/dtrun/controller/TaskController.java
@@ -20,8 +20,8 @@ public class TaskController {
     @GetMapping(value = "/getCos")
     public void getCosAllInputStream()
     {
-        String secretId = "AKIDHI6lDTvArb7OTQxFmYzFLM8ewyxh9PWw";
-        String secretKey = "MTK3f3D6dR1MTMfmbkeJiphAMZbfUdXt";
+        String secretId = "12312";
+        String secretKey = "123";
         COSCredentials cred = new BasicCOSCredentials(secretId, secretKey);
         // 2 设置 bucket 的区域, COS 地域的简称请参照 https://cloud.tencent.com/document/product/436/6224
 // clientConfig 中包含了设置 region, https(默认 http), 超时, 代理等 set 方法, 使用可参见源码或者常见问题 Java SDK 部分。
