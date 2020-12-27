e6f895fc65a7d4d38b8589dd992e0ffb5e012003
小轩 <709027500@qq.com>
2020-12-27 17:40:36 +0800

    迁移任务前端页面初步构建

--- src/main/react/src/components/movetask/Create.jsx
+++ src/main/react/src/components/movetask/Create.jsx
@@ -1,6 +1,7 @@
-import {Button, Checkbox, Form, message, Select, Steps} from 'antd';
+import {Button, Checkbox, Form, Input, Select, Steps, Tooltip} from 'antd';
 import Nav from '../common/Nav';
 import React from 'react';
+import {DiffTwoTone} from '@ant-design/icons';
 
 const { Step } = Steps;
 const { Option } = Select;
@@ -13,62 +14,7 @@ const layout = {
   },
 };
 
-const steps = [
-  {
-    title: '选择起始数据源',
-    content: (<Form
-      {...layout}
 
-      name="basic"
-
-
-    >
-
-      <Form.Item
-        label="数据源"
-        name="dtsName"
-      >
-        <Select
-          placeholder="请选择数据源"
-
-          allowClear
-        >
-          <Option value="cos">COS</Option>
-          <Option value="oss">OSS</Option>
-          <Option value="obs">OBS</Option>
-          <Option value="bos">BOS</Option>
-        </Select>
-      </Form.Item>
-      <Form.Item name="dtsType" label="Bucket" rules={[{ required: true, message: '请选择Bucket' }]}>
-        <Select
-          placeholder="请选择Bucket"
-          allowClear
-        >
-        </Select>
-      </Form.Item>
-
-      <Form.Item label="全量迁移">
-
-        <Checkbox></Checkbox>
-
-
-
-      </Form.Item>
-
-
-
-
-    </Form>),
-  },
-  {
-    title: '选择目的数据源',
-    content: 'Second-content',
-  },
-  {
-    title: '参数设置',
-    content: <div>123</div>,
-  },
-];
 
 const Create = () => {
   const [current, setCurrent] = React.useState(0);
@@ -80,6 +26,142 @@ const Create = () => {
   const prev = () => {
     setCurrent(current - 1);
   };
+
+  const srcOnFinish = values => {
+    console.log('Success:', values);
+    setCurrent(current + 1);
+  };
+
+  const desOnFinish = values => {
+    console.log('Success:', values);
+    setCurrent(current + 1);
+  };
+
+  const OnFinish = values => {
+    console.log('Success:', values);
+  };
+
+  const steps = [
+    {
+      title: '选择起始数据源',
+      content: (<Form
+        {...layout}
+        name="basic"
+        onFinish={srcOnFinish}
+      >
+        <Form.Item
+          label="数据源"
+          name="src"
+          rules={[{ required: true, message: '请选择数据源' }]}
+        >
+          <Select
+            placeholder="请选择数据源"
+            allowClear
+          >
+            <Option value="cos">COS</Option>
+            <Option value="oss">OSS</Option>
+            <Option value="obs">OBS</Option>
+            <Option value="bos">BOS</Option>
+          </Select>
+        </Form.Item>
+        <Form.Item name="dtsType" label="Bucket" >
+          <Select
+            placeholder="请选择Bucket"
+            allowClear
+          >
+          </Select>
+        </Form.Item>
+
+        <Form.Item label="全量迁移" name="allmove">
+          <Checkbox defaultChecked='true'></Checkbox>
+        </Form.Item>
+
+        <Form.Item>
+          <Button type="primary" htmlType="submit" style={{ marginLeft: '25%' }}>
+            下一步
+          </Button>
+        </Form.Item>
+      </Form>),
+    },
+    {
+      title: '选择目的数据源',
+      content: (<Form
+        {...layout}
+        name="basic"
+        onFinish={desOnFinish}
+      >
+        <Form.Item
+          label="数据源"
+          name="des"
+          rules={[{ required: true, message: '请选择数据源' }]}
+        >
+          <Select
+            placeholder="请选择数据源"
+            allowClear
+          >
+            <Option value="cos">COS</Option>
+            <Option value="oss">OSS</Option>
+            <Option value="obs">OBS</Option>
+            <Option value="bos">BOS</Option>
+          </Select>
+        </Form.Item>
+        <Form.Item name="dtsType" label="Bucket" >
+          <Input.Group compact>
+            <Select
+              placeholder="请选择Bucket"
+              allowClear
+              style={{ width: '95%', marginTop: '2px' }}
+            >
+            </Select>
+            <Tooltip title="创建Bucket">
+              <Button type="text" style={{ marginLeft: '1px', width: '5%', background: 'white' }} icon={<DiffTwoTone style={{ fontSize: '25px' }} />}></Button>
+            </Tooltip>
+          </Input.Group>
+        </Form.Item>
+
+        <Form.Item >
+          <Button type="primary" htmlType="submit" style={{ margin: '0 10px 0 25%' }}>
+            下一步
+          </Button>
+          <Button type="primary" htmlType="button" onClick={() => prev()}>
+            上一步
+          </Button>
+        </Form.Item>
+      </Form>),
+    },
+    {
+      title: '参数设置',
+      content: (<Form
+        {...layout}
+        name="basic"
+        onFinish={OnFinish}
+      >
+        <Form.Item
+          label="任务名称"
+          name="taskname"
+          rules={[{ required: true, message: '请选择Bucket' }]}
+        >
+          <Input placeholder="请输入任务名称" />
+        </Form.Item>
+        <Form.Item name="dtsType" label="Bucket" >
+          <Select
+            placeholder="请选择Bucket"
+            allowClear
+            style={{ width: '95%', marginTop: '2px' }}
+          >
+          </Select>
+        </Form.Item>
+        <Form.Item >
+          <Button type="primary" htmlType="submit" style={{ margin: '0 10px 0 25%' }}>
+            确定
+          </Button>
+          <Button type="primary" htmlType="button" onClick={() => prev()}>
+            上一步
+          </Button>
+        </Form.Item>
+      </Form>),
+    },
+  ];
 
 
   return <div className="content-wrapper">
@@ -95,21 +177,7 @@ const Create = () => {
         </Steps>
         <div className="steps-content">{steps[current].content}
           <div className="steps-action" style={{ marginLeft: '17%' }}>
-            {current < steps.length - 1 && (
-              <Button type="primary" onClick={() => next()}>
-                下一步
-              </Button>
-            )}
-            {current === steps.length - 1 && (
-              <Button type="primary" onClick={() => message.success('Processing complete!')}>
-                确定
-              </Button>
-            )}
-            {current > 0 && (
-              <Button style={{ margin: '0 8px' }} onClick={() => prev()}>
-                上一步
-              </Button>
-            )}
+
           </div>
         </div>
 
